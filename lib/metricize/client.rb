652884d497cf15d855b8fab96ad87a4b8476e7ca
Matt McNeil <mmcneil@liveworld.com>
2013-07-08 09:56:16 -0700

    reduce logging verbosity at :info level

--- lib/metricize/client.rb
+++ lib/metricize/client.rb
@@ -42,7 +42,7 @@ module Metricize
       @redis.lpush(@queue_name, data)
       msg = "#{name.gsub('.', '_')}=#{value}" # splunk chokes on dots in field names
       msg << ", metric_source=#{options[:source].gsub('.', '_')}" if options[:source]
-      log_message msg, :info
+      log_message msg
     end
 
     def build_metric_name(name)
